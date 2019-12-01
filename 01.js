3f4a8f0e10967cee09c29e8ea1e5e163d0e49639
Tobias Jansson <tobias.jansson@gmail.com>
2019-12-01 14:54:56 +0100

    Städade upp filinläsningen

--- 01.js
+++ 01.js
@@ -1,29 +1,20 @@
-var fs = require('fs');
-var args = process.argv.slice(2);
+const fs = require('fs');
+const args = process.argv.slice(2);
 
-function read(file, parse) {
-    fs.readFile(file, 'utf8', function (err, data) {
-        if (err) {
-            console.log(err);
-        }
-        parse(data);
-    });
+const data = fs.readFileSync(args[0], 'utf8');
+
+let lines = data.split('\n');
+let totalfuel = 0;
+for (line of lines) {
+    let mass = parseFloat(line);
+    totalfuel += calcfuel(0, mass);
 }
-
-read(args[0], function (data) {
-    let lines = data.split('\n');
-    let totalfuel = 0;
-    for(line of lines) {
-        let mass = parseFloat(line);
-        totalfuel += calcfuel(0, mass);
-    }
-    console.log(totalfuel);
-});
+console.log(totalfuel);
 
 function calcfuel(total, mass) {
     let fuel = (Math.floor(mass / 3)) - 2;
-    if(fuel > 0) {
+    if (fuel > 0) {
         return calcfuel(total + fuel, fuel);
-    }   
+    }
     return total;
 }
