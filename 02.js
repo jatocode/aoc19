ba98d5b87a7df8402191863c8f63c307eee255ab
Tobias Jansson <tobias.jansson@prevas.se>
2019-12-02 09:49:13 +0100

    Dag 2, del 2

--- 02.js
+++ 02.js
@@ -4,29 +4,44 @@ const args = process.argv.slice(2);
 const data = fs.readFileSync(args[0], 'utf8');
 
 let lines = data.split('\n');
-let program = lines[0].split(',').map(x => parseInt(x));
-let running = true;
-let a, b, c = 0;
-while (running) {
-    let opcode = program[c];
-    switch (opcode) {
-        case 1:
-            a = program[program[c + 1]];
-            b = program[program[c + 2]];
-            program[program[c + 3]] = a + b;
-            c += 4;
-            break;
-        case 2:
-            a = program[program[c + 1]];
-            b = program[program[c + 2]];
-            program[program[c + 3]] = a * b;
-            c += 4; break;
-        case 99:
-            running = false;
-            break;
-        default:
-            c++;
-            break;
+const orgprogram = lines[0].split(',').map(x => parseInt(x));
+
+for (let noun = 0; noun < 100; noun++) {
+    for (let verb = 0; verb < 100; verb++) {
+        const copy = [...orgprogram];
+        let output = computer(copy, noun, verb);
+        if(output == 19690720 || output == 4945026) {
+            console.log(output, noun, verb, (100*noun + verb));
+        }
     }
 }
-console.log(program[0]);
+
+function computer(program, noun, verb) {
+    program[1] = noun;
+    program[2] = verb;
+    let a, b, c = 0;
+    running = true;
+    while (running) {
+        let opcode = program[c];
+        switch (opcode) {
+            case 1:
+                a = program[program[c + 1]];
+                b = program[program[c + 2]];
+                program[program[c + 3]] = a + b;
+                c += 4;
+                break;
+            case 2:
+                a = program[program[c + 1]];
+                b = program[program[c + 2]];
+                program[program[c + 3]] = a * b;
+                c += 4; break;
+            case 99:
+                running = false;
+                break;
+            default:
+                c++;
+                break;
+        }
+    }
+    return program[0];
+}
