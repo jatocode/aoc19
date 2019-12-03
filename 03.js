c4ea812296106fe3a253726987f92b124d1f82e8
Tobias Jansson <tobias.jansson@prevas.se>
2019-12-03 09:31:18 +0100

    Del 2 påbörjad

--- 03.js
+++ 03.js
@@ -9,15 +9,19 @@ wire1 = lines[0].split(',');
 wire2 = lines[1].split(',');
 
 grid = [];
-grid['0,0'] = 'o';
+grid['0,0'] = {n:'o', s: 0};
 
 tracewire(wire1, '1');
-console.log('Closest cross distance: ' + tracewire(wire2, '2'));
+let cross = tracewire(wire2, '2');
+console.log('Del 1: Closest cross distance: ' + cross[0]);
+console.log('Del 2: Closest cross steps: ' + cross[1]);
 
 function tracewire(wire, wirename) {
     let x = 0;
     let y = 0;
     let mincross = Number.MAX_SAFE_INTEGER;
+    let minsteps = Number.MAX_SAFE_INTEGER;
+    let ts = 0;
     for (step of wire) {
         let dir, steps;
         [_, dir, steps] = step.match(/([A-Z])(\d+)/);
@@ -31,16 +35,17 @@ function tracewire(wire, wirename) {
         for (let i=0; i < parseInt(steps); i++) {
             x += d[0];
             y += d[1];
+            ts++;
             if(grid[`${x},${y}`] == undefined) {
-                grid[`${x},${y}`] = wirename;
+                grid[`${x},${y}`] = {n:wirename, s: ts};
             } else if(grid[`${x},${y}`] == wirename) { 
                 // Crossed myself
             } else {
-                // console.log(`Cross at ${x},${y}`);
                 let dist = Math.abs(x) + Math.abs(y);
                 mincross = mincross > dist ? dist: mincross;
+                minsteps = minsteps > grid[`${x},${y}`].s + ts ? grid[`${x},${y}`].s + ts : minsteps;
             }
         }
     }
-    return mincross;
+    return [mincross, minsteps];
 }
