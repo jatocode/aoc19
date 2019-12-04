641eff47debc6157d8e9a010065e69a71dc1b140
Tobias Jansson <tobias.jansson@prevas.se>
2019-12-04 09:27:49 +0100

    Dag 4 del 2. Regexp magic

--- 04.js
+++ 04.js
@@ -1,15 +1,15 @@
 const input = '236491-713787';
 
-// Test...
+// Test 1
 // console.log(valid('111111'));
 // console.log(valid('223450'));
 // console.log(valid('123789'));
 // console.log(valid('111123'));
-// console.log(valid('255148'));
-// console.log(valid('133697'));
-// console.log(valid('244248'));
-// console.log(valid('242200'));
-// console.log(valid('239116'));
+
+// Test 2
+console.log(valid('112233'));
+console.log(valid('123444'));
+console.log(valid('111122'));
 
 let n = 0;
 for (let i = 236491; i < 713787 + 1; i++) {
@@ -17,19 +17,37 @@ for (let i = 236491; i < 713787 + 1; i++) {
         n++;
     }
 }
-console.log('Del 1: Number of pwd: ' + n);
+console.log('Del 2: Number of pwd: ' + n);
 
 function valid(ti) {
     for (let i = 0; i < ti.length - 1; i++) {
         const a = parseInt(ti[i]);
         const b = parseInt(ti[i + 1]);
-        if(a > b) {
+        if (a > b) {
             return false;
         }
     }
-    let repeats = ti.match(/(.)\1/);
-    if(repeats) { 
-        return true;
+    // Find repeating
+    let repeats = ti.match(/(.)\1{2,}/g);
+
+    // Find doubles only
+    let doubles = ti.match(/(.)\1/g);
+    if (doubles) {
+        if (repeats) {
+            // Städa bort de som ingår i repeterade
+            doubles = doubles.filter(d => {
+                for (r of repeats) {
+                    if (r.startsWith(d)) {
+                        return false;
+                    }
+                }
+                return true;
+            });
+        }
+        if (doubles.length > 0) {
+            return true;
+        }
     }
+
     return false;
 }
