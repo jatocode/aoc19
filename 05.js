d9bde7b71603b15110d8f3389e147afd06dde1cd
Tobias Jansson <tobias.jansson@prevas.se>
2019-12-05 10:21:58 +0100

    Dag 5, del 1. Fick till det trots att jag inte riktigt begrep

--- 05.js
+++ 05.js
@@ -13,40 +13,44 @@ function computer(program, input) {
     let running = true;
     while (running) {
         let instruction = program[pc].toString();
-        let len = instruction.length  - 1;
-        let opcode = parseInt(instruction[len]);
- 
-        let modeC = instruction[len - 2] == undefined ? 0 :  parseInt(instruction[len - 2]);
-        let modeB = instruction[len - 3] == undefined ? 0 :  parseInt(instruction[len - 3]);
-        let modeA = instruction[len - 4] == undefined ? 0 :  parseInt(instruction[len - 4]);
+        let len = instruction.length - 1;
+        let o2 = instruction[len - 1] == undefined ? '0' : instruction[len - 1];
+        let opcode = parseInt(o2 + instruction[len]);
+
+        let modeC = instruction[len - 2] == undefined ? 0 : parseInt(instruction[len - 2]);
+        let modeB = instruction[len - 3] == undefined ? 0 : parseInt(instruction[len - 3]);
+        let modeA = instruction[len - 4] == undefined ? 0 : parseInt(instruction[len - 4]);
+        //console.log('>', pc, opcode, modeC, modeB, modeA);
         switch (opcode) {
             case 1:
                 a = getValue(program, pc + 1, modeC);
                 b = getValue(program, pc + 2, modeB);
-                setValue(program, pc + 3, modeA, a + b);
+                program[program[pc + 3]] = a + b;
                 pc += 4;
                 break;
             case 2:
                 a = getValue(program, pc + 1, modeC);
                 b = getValue(program, pc + 2, modeB);
-                setValue(program, pc + 3, modeA,  a * b);
-                pc += 4; 
+                program[program[pc + 3]] = a * b;
+                pc += 4;
                 break;
             case 3:
-                a = getValue(program, pc + 1, modeC);
-                setValue(program, a, modeA, input);
+                a = program[pc + 1];
+                console.log(`Setting pc at ${a} to ${input}`);
+                program[a] = input;
                 pc += 2;
                 break;
             case 4:
                 a = getValue(program, pc + 1, modeC);
-                console.log(`Output: ${a}`);
+                a = program[pc + 1];
+                console.log(`Output (${pc}): ${program[a]}`);
                 pc += 2;
                 break;
             case 99:
                 running = false;
                 break;
             default:
-                pc++;
+                console.log('?', opcode);
                 break;
         }
     }
@@ -54,21 +58,10 @@ function computer(program, input) {
 }
 
 function getValue(program, pc, mode) {
-    switch(mode) {
+    switch (mode) {
         case 0: // Position mode
             return program[program[pc]];
         case 1: // Immediate mode
             return program[pc];
-    }
-}
-
-function setValue(program, pc, mode, value) {
-    switch(mode) {
-        case 0: // Position mode
-            program[program[pc]] = value;
-            break;
-        case 1: // Immediate mode
-            program[pc + 1] = value;
-            break;
     }
 }
