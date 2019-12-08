fbcba807afddad86d421f25572ffa43138289c23
Tobias Jansson <tobias.jansson@gmail.com>
2019-12-08 16:40:50 +0100

    Dag 8, del 2

--- 08.js
+++ 08.js
@@ -6,8 +6,27 @@ let lines = data.split('\n');
 
 const imagedata = lines[0].split('');
 
-// decode('123456789012', 3, 2);
-const image = decode(imagedata, 25, 6);
+const layers = decode(imagedata, 25, 6);
+console.log(display(layers));
+
+function display(layers) {
+    let image = '';
+    for (let h = 0; h < layers[0].length; h++) {
+        let row = '';
+        for (let w = 0; w < layers[0][0].length; w++) { 
+            let pixel = 2;
+            for (const layer of layers) {
+                if(layer[h][w] != 2) {
+                    pixel = layer[h][w] == 0 ? ' ' : '🎅';
+                    break;
+                } 
+            }
+            row += pixel.toString();
+        }
+        image += row + '\n';
+    }
+    return image;
+}
 
 function decode(imagedata, width, height) {
     let image = [];
@@ -24,7 +43,7 @@ function decode(imagedata, width, height) {
             let row = [];
             for (let w = 0; w < width; w++) {
                 let d = parseInt(imagedata[i++]);
-                switch(d) {
+                switch (d) {
                     case 0: num0++; break;
                     case 1: num1++; break;
                     case 2: num2++; break;
@@ -33,15 +52,16 @@ function decode(imagedata, width, height) {
             }
             layer.push(row);
         }
-        layerinfo.push({num0:num0, num1:num1, num2: num2});
-        if(num0 < min0) {
+        layerinfo.push({ num0: num0, num1: num1, num2: num2 });
+        if (num0 < min0) {
             min0 = num0;
             min0layer = l;
         }
         l++;
         image.push(layer);
     }
-    console.log(layerinfo[min0layer], 'Del 1:', layerinfo[min0layer].num1 * layerinfo[min0layer].num2);
+    console.log('Del 1:', layerinfo[min0layer].num1 * layerinfo[min0layer].num2);
+    console.log();
     return image;
 }
 
