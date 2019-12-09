43fcbe87669cb09c94e5d1316fe849af075b8dd4
Tobias Jansson <tobias.jansson@prevas.se>
2019-12-09 10:37:58 +0100

    Refaktorerade 9 lite för skoj skull

--- 09.js
+++ 09.js
@@ -11,17 +11,27 @@ const inputprogram = lines[0].split(',').map(x => parseInt(x));
 // computer([1102,34915192,34915192,7,4,7,99,0], 0);
 // computer([104,1125899906842624,99],0);
 
+console.log('Del 1:', intcode(inputprogram, () => { return 1}, () => {}));
+console.log('Del 2:', intcode(inputprogram, () => { return 2}, () => {}));
 
-console.log('Del 1: ');
-computer(inputprogram, 1)
-
-console.log('Del 2: ');
-computer(inputprogram, 2);
-
-function computer(program, input) {
+function intcode(program, input = () => { return 0}, output = (x) => { console.log(`Output: ${x}`) } ) {
     let a, b, o, pc = 0;
     let rb = 0;
     let running = true;
+    let lastout = undefined;
+
+    // Två hjälpmetoder
+    let get = (pc) => {
+        return program[pc] != undefined ? program[pc] : 0;
+    }
+    let getPointer = (pc, mode) => {
+        switch (mode) {
+            case 0: return program[pc];
+            case 1: return pc; 
+            case 2: return rb + program[pc];
+        }  
+    }
+
     while (running) {
         let instruction = program[pc].toString();
         let len = instruction.length - 1;
@@ -32,43 +42,44 @@ function computer(program, input) {
         let modeB = instruction[len - 3] == undefined ? 0 : parseInt(instruction[len - 3]);
         let modeA = instruction[len - 4] == undefined ? 0 : parseInt(instruction[len - 4]);
 
-        a = getPointer(pc + 1, program, rb, modeC);
-        b = getPointer(pc + 2, program, rb, modeB);
-        o = getPointer(pc + 3, program, rb, modeA);
+        a = getPointer(pc + 1, modeC);
+        b = getPointer(pc + 2, modeB);
+        o = getPointer(pc + 3, modeA);
 
         switch (opcode) {
             case 1:
-                program[o] = get(a, program) + get(b, program);
+                program[o] = get(a) + get(b);
                 pc += 4;
                 break;
             case 2:
-                program[o] = get(a, program) * get(b, program);
+                program[o] = get(a) * get(b);
                 pc += 4;
                 break;
             case 3:
-                program[a] = input;
+                program[a] = input();
                 pc += 2;
                 break;
             case 4:
-                console.log(`Output (${pc}): ${program[a]}`);
+                lastout = program[a];
+                output(lastout);
                 pc += 2;
                 break;
             case 5:
-                if (get(a, program) != 0) {
-                    pc = get(b, program)
+                if (get(a) != 0) {
+                    pc = get(b)
                 } else {
                     pc += 3;
                 }
                 break;
             case 6:
-                if (get(a, program) == 0) {
-                    pc = get(b, program);
+                if (get(a) == 0) {
+                    pc = get(b);
                 } else {
                     pc += 3;
                 }
                 break;
             case 7:
-                if (get(a, program) < get(b, program)) {
+                if (get(a) < get(b)) {
                     program[o] = 1;
                 } else {
                     program[o] = 0;
@@ -76,7 +87,7 @@ function computer(program, input) {
                 pc += 4;
                 break;
             case 8:
-                if (get(a, program) == get(b, program)) {
+                if (get(a) == get(b)) {
                     program[o] = 1;
                 } else {
                     program[o] = 0;
@@ -84,7 +95,7 @@ function computer(program, input) {
                 pc += 4;
                 break;
             case 9:
-                rb += get(a, program);
+                rb += get(a);
                 pc +=2;
                 break;
             case 99:
@@ -95,18 +106,5 @@ function computer(program, input) {
                 break;
         }
     }
-    return program[0];
-}
-
-function get(pc, program) {
-    return program[pc] != undefined ? program[pc] : 0;
-}
-
-function getPointer(pc, program, rb, mode) {
-    switch (mode) {
-        case 0: return program[pc];
-        case 1: return pc; 
-        case 2: return rb + program[pc];
-    }
-
+    return lastout;
 }
