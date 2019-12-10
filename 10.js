7d5346580592ffefa5282b0f1578e87e9c2dc772
Tobias Jansson <tobias.jansson@gmail.com>
2019-12-10 22:06:52 +0100

    Dag 10, del 1. Fick kämpa lite

--- 10.js
+++ 10.js
@@ -10,67 +10,45 @@ for (line of lines) {
     asteroids.push(line.trim().split(''));
 }
 
-console.table(asteroids);
+findBest();
 
-console.log(calcLines(3,4));
-console.log(calcLines(1,0));
-
-let map = [];
-for (let y = 0; y < asteroids[0].length; y++) {
-    const arow = asteroids[y];
-    for (let x = 0; x < arow.length; x++) {
-        const asteroid = arow[x];   
-        if (asteroid != '.') {
-            const l = calcLines(x,y);
-            console.log(x,y,l);
-            map[`${x}${y}`] = l;
-            arow[x] = l;
-        }          
+function findBest() {
+    let best = { a: '', detected: 0 };
+    for (let y = 0; y < asteroids[0].length; y++) {
+        const arow = asteroids[y];
+        for (let x = 0; x < arow.length; x++) {
+            if (arow[x] != '.') {
+                const l = calcLines(x, y);
+                if (l > best.detected) {
+                    best.detected = l;
+                    best.a = `${x},${y}`;
+                }
+            }
+        }
     }
+    console.log(best);
 }
-console.table(map);
-console.table(asteroids);
-
-console.log(calcLines(4,2));
-
 
 function calcLines(fx, fy) {
-    let los = [];
     let linjer = new Set();
     for (let y = 0; y < asteroids[0].length; y++) {
-        const element = asteroids[y];
-        let nr = [];
-        for (let x = 0; x < element.length; x++) {
-            let g = findLoS(fx, fy, x, y);
-            const asteroid = element[x];
-            if (asteroid != '.') {
-                nr.push(g);
+        const arow = asteroids[y];
+        for (let x = 0; x < arow.length; x++) {
+            if (arow[x] == '#') {
+                let g = findLoS(fx, fy, x, y);
                 linjer.add(g);
-            } else {
-                nr.push(null);
-            }
-
+            } 
         }
-        los.push(nr);
     }
-    console.table(los);
-    console.log(linjer);
-    //console.log(los);
-    return linjer.size;
+    return linjer.size - 1;
 }
-//console.table(asteroids);
 
 function findLoS(x1, y1, x2, y2) {
-    let xd = Math.abs(x2) - Math.abs(x1);
-    let yd = Math.abs(y2) - Math.abs(y1);
-    return xd / yd;
+    let xd = Math.abs(x2 - x1);
+    let yd = Math.abs(y2 - y1);
+
+    if (x1 == x2 && y1 == y2) return 'X';
+    let h = x1 - x2 < 0 ? 'R' : 'D';
+    let v = y1 - y2 < 0 ? 'D' : 'U';
+    return `${h}${v}-${yd/xd}`
 }
-
-/*
-  3 - 3  / 4 - 4  = 0 / 0 -> NaN
-  3 - 4 / 4 -4 = -1 / 0 
-
-  3 -2 / 4 -4 = 1 / 0
-
-
-*/
