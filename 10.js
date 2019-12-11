be265014cb72f9749f3652284230c7e2e00e7314
Tobias Jansson <tobias.jansson@gmail.com>
2019-12-11 01:09:34 +0100

    Får inte ordning på  del 2

--- 10.js
+++ 10.js
@@ -10,15 +10,38 @@ for (line of lines) {
     asteroids.push(line.trim().split(''));
 }
 
-findBest();
+//findBest();
+
+let result = calcLines(8,3);
+targets = result[1];
+
+let currentIndex = targets.findIndex(x => x.deg == -90);
+let hit = 1;
+let current = targets[currentIndex % targets.length];
+let prev = undefined;
+while(hit < 205) {
+    current = targets[currentIndex % targets.length];
+    currentIndex++;
+
+    if(prev != undefined && current.deg == prev.deg) continue; // Samma stråle
+    if(current.hit > 0) continue;
+    prev = current;
+    current.hit = hit++;
+    asteroids[current.y][current.x] = current.hit;
+    if(current.hit == 200) console.log(current);
+}
+//console.table(asteroids);
+
+//console.table(asteroids);
+//findBest(); // Del 1
 
 function findBest() {
     let best = { a: '', detected: 0 };
-    for (let y = 0; y < asteroids[0].length; y++) {
+    for (let y = 0; y < asteroids.length; y++) {
         const arow = asteroids[y];
         for (let x = 0; x < arow.length; x++) {
             if (arow[x] != '.') {
-                const l = calcLines(x, y);
+                const l = calcLines(x, y)[0];
                 if (l > best.detected) {
                     best.detected = l;
                     best.a = `${x},${y}`;
@@ -31,24 +54,27 @@ function findBest() {
 
 function calcLines(fx, fy) {
     let linjer = new Set();
-    for (let y = 0; y < asteroids[0].length; y++) {
+    let targets = [];
+    for (let y = 0; y < asteroids.length; y++) {
         const arow = asteroids[y];
         for (let x = 0; x < arow.length; x++) {
             if (arow[x] == '#') {
-                let g = findLoS(fx, fy, x, y);
-                linjer.add(g);
+                let dg = calcLoS(fx, fy, x, y);
+                linjer.add(dg);
+
+                // Bygg lista av mål
+                targets.push({deg: dg, x:x, y:y, dist: Math.abs(fx-x) + Math.abs(fy-y), hit: 0});
             } 
         }
     }
-    return linjer.size - 1;
+
+    // Sortera på grad och sen avstånd
+    targets = targets.sort((a,b) => a.deg - b.deg || a.dist - b.dist);
+
+    return [linjer.size - 1, targets];
 }
 
-function findLoS(x1, y1, x2, y2) {
-    let xd = Math.abs(x2 - x1);
-    let yd = Math.abs(y2 - y1);
-
+function calcLoS(x1, y1, x2, y2) {
     if (x1 == x2 && y1 == y2) return 'X';
-    let h = x1 - x2 < 0 ? 'R' : 'D';
-    let v = y1 - y2 < 0 ? 'D' : 'U';
-    return `${h}${v}-${yd/xd}`
+    return Math.atan2(y2 - y1, x2 - x1) * (180 / Math.PI);
 }
