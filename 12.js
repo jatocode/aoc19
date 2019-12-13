640342ecba12dce0cbd68577e374ef550a96d639
Tobias Jansson <tobias.jansson@prevas.se>
2019-12-13 14:32:39 +0100

    Försökte med 12.2 igen

--- 12.js
+++ 12.js
@@ -1,5 +1,4 @@
 const fs = require('fs');
-var ctx = require('axel');
 const args = process.argv.slice(2);
 
 const data = fs.readFileSync(args[0], 'utf8');
@@ -7,27 +6,28 @@ const data = fs.readFileSync(args[0], 'utf8');
 let lines = data.split('\n');
 let planets = [];
 let vel = [];
+
+let visitedX = new Set();
+let visitedY = new Set();
+let visitedZ = new Set();
+let cycleX  = 0;
+let cycleY  = 0;
+let cycleZ  = 0;
+
 for (line of lines) {
     let x = 0, y = 0, z = 0;
     [_, x, y, z] = line.match(/<x=(.?\d+), y=(.?\d+), z=(.?\d+)>/);
     planets.push({ x: parseInt(x), y: parseInt(y), z: parseInt(z), path: [] });
     vel.push({ x: 0, y: 0, z: 0 });
 }
-//display();
-ctx.bg(0,0,0);
-ctx.clear();
 
-
-for (let time = 1; time < 1001; time++) {
-    gravity();
+for (let time = 0; time < 3000; time++) {
+    gravity(time);
     addVelocity();
-    // if (time == 1000) {
-    //     console.log(`After ${time} steps`);
-    //     display();
-    //     displayEnergy();
-    // }
+   //console.log(cycleX, cycleY, cycleZ);
+    let g = gcd(cycleX, gcd(cycleY, cycleZ));
+   if(time == 1000 || time == 2770 || time == 2772) console.log(g, cycleX, cycleY, cycleZ);
 }
-ctx.cursor.restore();
 
 function displayEnergy() {
     let energy = [];
@@ -45,50 +45,61 @@ function displayEnergy() {
     console.log('Sum of total energy:' + total);
 }
 
-function gravity() {
+function gravity(time) {
+    let xv = '';
+    let yv = '';
+    let zv = '';
     for (let p1i = 0; p1i < planets.length; p1i++) {
         const p1 = planets[p1i];
+        xv += p1.x + ':' + vel[p1i].x + ' '; 
+        yv += p1.y + ':' + vel[p1i].y + ' ';
+        zv += p1.z + ':' + vel[p1i].z + ' ';
         for (let p2i = 0; p2i < planets.length; p2i++) {
             if (p1i == p2i) continue;
             const p2 = planets[p2i];
             vel[p1i].x += cmp(p1.x, p2.x);
             vel[p1i].y += cmp(p1.y, p2.y);
-            vel[p1i].z += cmp(p1.z, p2.z);
+            vel[p1i].z += cmp(p1.z, p2.z);           
         }
     }
+
+    if(time == 0 || time == 2770 || time == 2772) console.log(time,xv,yv,zv);
+    if(visitedX.has(xv)) {
+        cycleX = time;
+    } 
+    visitedX.add(xv);
+    if(visitedY.has(yv)) {
+        cycleY = time;
+    }
+    visitedY.add(yv);
+    if(visitedZ.has(zv)) {
+        cycleZ = time;
+    } 
+    visitedZ.add(zv);
 }
 
-function addVelocity() {
-    const colors = [[0, 128, 255], [0, 255, 128], [255, 128, 0], [255, 0, 255], [0, 0, 255]];
+function addVelocity(time) {
     for (let pi = 0; pi < planets.length; pi++) {
         const p = planets[pi];
         p.x += vel[pi].x;
         p.y += vel[pi].y;
         p.z += vel[pi].z;
 
-        p.path.unshift([(p.x / 4), (p.y / 4), (p.z)]);
-        p.path = p.path.slice(0,20);
-        const c = colors[pi];
-        for (const point of p.path) {
-            ctx.bg(fade(c[0], point[2]), fade(c[1], point[2]), fade(c[2], point[2]));
-            ctx.point(20+point[0], 20+point[1]);
-        }
-        if (p.path.length > 0) {
-            const point = p.path[p.path.length - 1];
-            //console.log(point);
-            ctx.bg(0, 0, 0);
-            ctx.point(20 + point[0], 20 +point[1]);
-        }
     }
-}
-
-function fade(c, z) {
-    let nc = (c - Math.abs(z*10));
-    return nc > 0 ? nc : 0;
+ 
 }
 
 function cmp(a, b) {
     if (a > b) return -1;
     if (a < b) return +1;
     return 0;
+}
+
+function gcd(a, b) {
+    // base cases
+    if(a === 0) { return b;}
+    if(b === 0) { return a;}
+
+    // decrease and conqure - recursion
+    return gcd(b, a % b);
 }
